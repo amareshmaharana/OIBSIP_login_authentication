a59abda96cad906e8d7c93250124b6e19a46da1c
Amaresh Maharana <amareshcoder@gmail.com>
2025-06-24 16:24:02 +0530

    style: format code and improve readability in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import cookieParser from 'cookie-parser';
-import { connectDB } from './config/mongodb.js';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./config/mongodb.js";
+import { router } from "./routes/auth.route.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-app.use(cors({credentials: true}));
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the backend server!');
+// API Endpoints
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend server!");
 });
+app.use("/api/auth", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
