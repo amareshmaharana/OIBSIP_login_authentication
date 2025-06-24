513c3379159c40bb80f2b1582aead45fc1443dca
Amaresh Maharana <amareshcoder@gmail.com>
2025-06-24 15:05:21 +0530

    feat: add user model with schema for authentication

---
+++ backend/models/user.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    verifyOTP: {
+      type: String,
+      default: "",
+    },
+    verifyOTPExpireAt: {
+      type: Number,
+      default: 0,
+    },
+    isAccountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetOTP: {
+      type: String,
+      default: "",
+    },
+    resetOTPExpireAt: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model.User || mongoose.model("User", userSchema);
+
+export default User;
